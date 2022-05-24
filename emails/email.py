6092d1edd64571b6538892fd9ed950ec81f92e78
Peter Kulisov <newfapman@gmail.com>
2022-05-24 16:06:56 +0100

    email extractor added

---
+++ emails/email.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+
+import re
+regex = r"^(([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,18}(?:\.[a-z]{2})?))$"
+
+path = "."
+files = os.listdir(path)
+print(files)
+files_xls = [f for f in files if f[-4:] == 'xlsx']
+files_xls.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+os.chdir(path)
+df_email = pd.DataFrame()
+for i in range(1): #Read the excel file with the most recent modified timestamp
+    print(files_xls[i])
+
+    xls = pd.ExcelFile(files_xls[i])
+    for sheet in xls.sheet_names:
+        df = pd.read_excel(files_xls[i], sheet_name=sheet)
+
+        for row in df.values.tolist():
+            for col in row:
+                matches = re.findall(regex, str(col))
+                if matches:
+                    df_email = df_email.append([matches[0]], ignore_index=True)
+
+
+
+df_email.to_excel("excel.xlsx")
