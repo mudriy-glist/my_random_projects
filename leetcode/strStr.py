eec812a2f6d151805e031d27e3f901023d0d71bf
Peter Kulisov <newfapman@gmail.com>
2022-07-25 13:55:22 +0100

    strStr added

---
+++ leetcode/strStr.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+    
+        res = haystack.find(needle)
+        return res
