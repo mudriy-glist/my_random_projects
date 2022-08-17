9cdf5173307288b72c4a7191a031aea4c7861a80
Peter Kulisov <newfapman@gmail.com>
2022-08-17 10:42:54 +0100

    big cleaning

--- leetcode/58_length_of_lastworld.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        s = (s.split())
-
-        return len(s[len(s) - 1])   
